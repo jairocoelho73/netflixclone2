e78406e65f211cdd0d697f0195217946e2a134db
jairocoelho73 <45605493+jairocoelho73@users.noreply.github.com>
2021-10-20 19:29:43 -0300

    Initial commit

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+import './Header.css';
+
+export default ({black}) => {
+    return (
+        <header className={black ? 'black' : ''}>
+            <div className="header--logo">
+                <a href="/">
+                    <img src="https://logodownload.org/wp-content/uploads/2014/10/netflix-logo.png" alt="Netflix" />
+                </a>
+            </div>
+        </header>
+    );
+}
